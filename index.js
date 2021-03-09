bac35c88960bd2429c43d4dbaa3684a253a0f1bb
Romario Gustave <romagustave10@gmail.com>
2021-03-09 05:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function testFunc() {
 
 
   const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
 ]
 function superbowlWin(){
   if (superbowlWin.find(result=>result === "W"))
