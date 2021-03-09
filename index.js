57357e0457c030fcf0033031c1021a1115d0820b
Romario Gustave <romagustave10@gmail.com>
2021-03-09 05:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ function testFunc() {
   return "hi"
 }
 
-function superbowlWin(){
+
   const record = [
   {year: "2018", result: "L"},
   {year: "2017", result: "W"},
   {year: "2016", result: "N/A"}
 ]
-if superbowlWin.find(result=>result === "W")
+function superbowlWin(){
+if (superbowlWin.find(result=>result === "W"))
 return year;
 else 
 return undefined;
